94627c868149884f8d4f9841b2c00fb3bf9ed2b4
Alexis <AlexDdevs@gmail.com>
2024-09-19 20:18:31 +0200

    🎨 Create a Footer component

---
+++ src/components/Footer.tsx
@@ -0,0 +1,15 @@
+export default function Header() {
+    return (
+        <footer className="flex items-center justify-between max-[1440px]:px-8 pb-5 max-w-[1440px] mx-auto">
+            <div className="bg-bgBlack font-bold text-xs text-purpleLight w-[91px] h-[27px] flex items-center justify-center uppercase rounded-[80px] rotate-6">
+                La piscine
+            </div>
+            <div className="font-bold text-xs text-bgBlack">
+                100%
+            </div>
+            <div className="font-bold text-xs text-bgBlack">
+                @2024
+            </div>
+        </footer>
+    )
+}
