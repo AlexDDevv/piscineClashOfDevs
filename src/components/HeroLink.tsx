59e1565a9c4ebdfa4b8278ea44b57b69ad887980
Alexis <AlexDdevs@gmail.com>
2024-09-21 10:11:12 +0200

    💫 Add animations for HeroLink

--- src/components/HeroLink.tsx
+++ src/components/HeroLink.tsx
@@ -1,12 +1,20 @@
 import Image from "next/image";
 import data from "../data/data.json"
+import { motion } from "framer-motion"
 
 export default function HeroLink({ showEasterEgg }: { showEasterEgg: boolean }) {
     return (
         <>
             {!showEasterEgg ? (
                 data.heroLinks.map(link => (
-                    <div key={link.content} className={link.classDiv}>
+                    <motion.div
+                        key={link.content}
+                        className={link.classDiv}
+                        initial={{ scale: 0, opacity: 0 }}
+                        animate={{ scale: 1, opacity: 1 }}
+                        exit={{ scale: 1, opacity: 1 }}
+                        transition={{ duration: 0.5, ease: "easeInOut", delay: 2 }}
+                    >
                         <a href={link.href} className={link.classLink} target="blank">{link.content}</a>
                         <Image
                             src={link.cursor}
@@ -15,7 +23,7 @@ export default function HeroLink({ showEasterEgg }: { showEasterEgg: boolean }) 
                             height={13}
                             className={link.classCursor}
                         />
-                    </div>
+                    </motion.div>
                 ))
             ) : (
                 data.easterEggLink.map(link => (
